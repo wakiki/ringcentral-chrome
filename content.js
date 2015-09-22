5448b6e4b3be702fb13737c3343c8023e56ff186
Suraj Pratap <surajpratap@icloud.com>
2015-09-22 18:26:04 +0530

    handle ajax

--- content.js
+++ content.js
@@ -14,79 +14,80 @@ document.head.appendChild (styleNode);
 var p33_ctd = {
 	ignoreElements: ['html','iframe','noscript','style','option','select','script','label','img','input','textarea','head','link'],
 	validExpressions: [/^((\+?1)?[-. ]?((\([2-9][0-8][0-9]\))|([2-9][0-8][0-9]))[-. ]?)?([2-9][0-9]{2})[-. ]?([0-9]{4})$/, /^\+[0-9]{1,3}\.[0-9]{4,14}$/, /^\+1\.[0-9]{7,15}$/,/[1\-]?[0-9]{3}(\-| |\.)[0-9]{3}(\-| |\.)[0-9]{4}/,/\([0-9]{3}\)(\-| |\.)[0-9]{3}(\-| |\.)[0-9]{4}/,/[0-9]{2}(\-| |\.)[0-9]{2}(\-| |\.)[0-9]{4}(\-| |\.)[0-9]{4}/]
+};
+
+var telephonised = function(){
+	var phoneNodes = jQuery('.phone-number:not(.telephonised)');
+	jQuery.each(phoneNodes, function(index, phoneNode){
+		var parent = jQuery( phoneNode );
+		var number = parent.html().replace(/\s/g, '');
+		var span = document.createElement("span");
+		span.className = "CTDphoneNumber";
+		jQuery(span).attr('data-phone', number);
+		parent.append(span);
+		jQuery(span).append('<i style="margin-left:5px;" class="fa fa-green fa-phone-square p33_ctd_btn"></i>');
+		jQuery(span).find('i').mouseover(function(){
+		});
+		jQuery(span).find('i').click(function(e){
+			addContextualMenu(e.target);
+			e.preventDefault();
+			return false;
+		});
+		parent.addClass('telephonised');
+	});
+};
+
+setInterval(function(){telephonised()}, 5000);
+
+var c = document.body.childNodes;
+var elements = [];
+for ( var i = 0; i < c.length; i++ ) {
+	elements[elements.length] = c[i]
 }
-
-window.telephonise = function() {
-	var c = document.body.childNodes;
-	var elements = [];
-	for ( var i = 0; i < c.length; i++ ) {
-		elements[elements.length] = c[i]
-	}
-	for ( var i = 0; i < elements.length; i++ ) {
-		if ( elements[i].nodeType == 1 ) {
-			for (var ii = 0; ii < elements[i].childNodes.length; ii++) {
-				elements[elements.length] = elements[i].childNodes[ii];
-			}
+for ( var i = 0; i < elements.length; i++ ) {
+	if ( elements[i].nodeType == 1 ) {
+		for (var ii = 0; ii < elements[i].childNodes.length; ii++) {
+			elements[elements.length] = elements[i].childNodes[ii];
 		}
-		if ( elements[i].nodeType == 3 ) {
-
-			if( jQuery(elements[i]).parents('.telephonised').length > 0 ){
-				continue
-			}
-
-			if ( p33_ctd.ignoreElements.indexOf(elements[i].parentNode.tagName.toLowerCase()) < 0 ) {
-				for( var ie = 0; ie < p33_ctd.validExpressions.length; ie++ ) {
-					var test = elements[i].textContent.match(p33_ctd.validExpressions[ie]);
-
-					var telephoneNode = false
-
-					if( /phone-number/.test(elements[i].parentNode.className) ){
-						test = elements[i].textContent.replace(/\s/g, '')
-						telephoneNode = true
+	}
+	if ( elements[i].nodeType == 3 ) {
+		if ( p33_ctd.ignoreElements.indexOf(elements[i].parentNode.tagName.toLowerCase()) < 0 ) {
+			for( var ie = 0; ie < p33_ctd.validExpressions.length; ie++ ) {
+				var test = elements[i].textContent.match(p33_ctd.validExpressions[ie]);
+				if ( test ) {
+					var telNum = '';
+					if ( typeof test[0] == 'string' ) {
+						telNum = test[0].replace(/[^0-9]/ig,'');
 					}
+					matchText(elements[i].parentNode, p33_ctd.validExpressions[ie], function(node, match, offset) {
+						var span = document.createElement("span");
+						span.className = "CTDphoneNumber";
+						jQuery(span).attr('data-phone',telNum);
+						span.textContent = match;
+						/*
+						 if (node.parentNode.tagName == 'A') {
+						 node.parentNode.parentNode.insertBefore(span, node.parentNode);
+						 } else {
+						 node.parentNode.insertBefore(span, node.nextSibling);
+						 }
+						 */
+						node.parentNode.insertBefore(span, node.nextSibling);
+						jQuery(span).append('<i style="margin-left:5px;" class="fa fa-green fa-phone-square p33_ctd_btn"></i>');
+						jQuery(span).find('i').mouseover(function(){
+						});
+						jQuery(span).find('i').click(function(e){
+							addContextualMenu(e.target);
+							e.preventDefault();
+							return false;
+						});
 
-					if ( test || telephoneNode) {
-						var telNum = '';
-						if(telephoneNode){
-							telNum = test
-							// throw(telNum)
-						}
-						if ( typeof test[0] == 'string' && !telephoneNode) {
-							telNum = test[0].replace(/[^0-9]/ig,'');
-						}
-						matchText(elements[i].parentNode, (telephoneNode ? /(?:.*)/ : p33_ctd.validExpressions[ie]), function(node, match, offset) {
-							var span = document.createElement("span");
-							span.className = "CTDphoneNumber";
-							jQuery(span).attr('data-phone',telNum);
-							span.textContent = match;
-
-							if(telephoneNode){
-								jQuery(node.parentNode).append(span)
-							}else{
-								node.parentNode.insertBefore(span, node.nextSibling);
-							}
-
-							jQuery(node.parentNode).addClass('telephonised')
-
-							jQuery(span).append('<i style="margin-left:5px;" class="fa fa-green fa-phone-square p33_ctd_btn"></i>');
-							jQuery(span).find('i').mouseover(function(){
-							});
-							jQuery(span).find('i').click(function(e){
-								addContextualMenu(e.target);
-								e.preventDefault();
-								return false;
-							});
-
-						},p33_ctd.ignoreElements);
-						break;
-					}
+					},p33_ctd.ignoreElements);
+					break;
 				}
 			}
 		}
 	}
 }
-
-setInterval(function(){telephonise()}, 5000)
 
 function initiateCall(targetObj) {
 	var phone_number = jQuery(targetObj.parentElement).attr('data-phone');
